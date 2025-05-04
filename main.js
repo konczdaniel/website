7e4082894b4dae677820c7ce5271fba3d28ef150
Koncz Daniel <konczdaniel6@gmail.com>
2025-05-04 21:16:57 +0300

    Fully working Home Page

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.querySelectorAll('.hamburger span').forEach(span => {
+      span.removeAttribute('data-aos');
+      span.removeAttribute('data-aos-duration');
+      span.style.transform = '';
+      span.style.opacity = '';
+    });
+  }, 1700);
+});
+
 function toggleMenu() {
-  const menu = document.getElementById('mobileMenu');
-  menu.classList.toggle('show');
+
+  hamburger.classList.toggle('show');
+  mobileMenu.classList.toggle('show');
 }
+
+// Optional: Close mobile menu when clicking a link
+document.querySelectorAll('.mobile-link').forEach(link => {
+  link.addEventListener('click', () => {
+    document.querySelector('.hamburger').classList.remove('show');
+    document.getElementById('mobileMenu').classList.remove('show');
+  });
+});
